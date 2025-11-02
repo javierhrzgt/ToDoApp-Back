149f6b409de56812ec2923fedaffe5c5b62fc590
javierhrzgt <eljavierhrz@gmail.com>
2025-11-01 20:01:35 -0600

    Completamos toda la parte del backend

---
+++ back/src/server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import notesRouter from "./routes/notesRoutes.js"
+import dotenv from "dotenv"
+import { connectDB } from "./config/db.js"
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use("/api/notes",notesRouter)
+
+const PORT = process.env.PORT || 3001
+
+connectDB()
+.then(()=>{
+    app.listen(PORT,() =>{
+        console.log(`Servidor levantado en http://localhost:${PORT}`)
+})
+})
